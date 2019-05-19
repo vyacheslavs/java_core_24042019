09eb7dee8ae7dda5310b65f024876e9590b0779d
Shegai Vyacheslav <v.shegai@netris.ru>
2019-05-20 02:06:35 +0300

    добавлены стили для чата

---
+++ src/main/java/Lesson_4/sample/Main.java
@@ -0,0 +1,36 @@
+package Lesson_4.sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    int chatWidth = 400;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        root.setId("pane");
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, chatWidth, 275));
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setX(bounds.getMaxX()-chatWidth);
+        primaryStage.setHeight(bounds.getHeight());
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
